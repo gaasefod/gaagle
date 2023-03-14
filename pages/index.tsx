a90b18005e6f250e797d006f626ea5229c89674c
gaasefod <gaasefod@tutanota.com>
2023-03-14 17:58:19 +0100

    Add Work Sans font

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import Style from "@/styles/Home.module.scss";
 import Head from "next/head";
 
+import { Work_Sans } from "next/font/google";
+
+const font = Work_Sans({ subsets: ["latin"] });
+
 const Home = (): JSX.Element => {
   return (
-    <div className={Style.home}>
+    <div className={`${Style.home} ${font.className}`}>
       <Head>
         <title>Gaagle</title>
       </Head>
